bf4fd313b1703ef466674f40eae49095bdcf2745
ducvv <ducvv@mor.com.vn>
2022-12-28 21:46:08 +0700

    nodemon

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3050
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World duc 50!')
 })
 
 app.listen(port, () => {
